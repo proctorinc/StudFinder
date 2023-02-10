4cbe9a9552b673f536cbe462dd533f6988f55613
Matt Proctor <proctormatt0@gmail.com>
2023-02-09 22:19:32 -0800

    Simplified Slider and removed react-slider

--- src/components/ui/Slider/Slider.jsx
+++ src/components/ui/Slider/Slider.jsx
@@ -1,36 +1,12 @@
-import ReactSlider from "react-slider"
-import styles from "./Slider.module.css"
-
 export const Slider = ({ value, min, max, onChange }) => {
-
-  const Track = () => {
-    return (
-      <div className={styles.track}>
-        {[...Array(max)].map((_, i) => <div key={i} className={styles.marking} />
-  )}
-      </div>
-    )
-  }
-
-  const Thumb = ({ className, otherProps }) => {
-    console.log(className)
-    return (
-        <img className={[styles.thumb, className]} src="stud-finder.png" alt="stud-finder" {...otherProps} />
-    )
-  }
-
   return (
-    <>
-      <Track />
-      <ReactSlider
-        className="horizontal-slider"
-        value={value}
-        min={min}
-        max={max}
-        step={1}
-        renderThumb={(props) => <Thumb {...props} />}
-        onChange={onChange}
-      />
-    </>
+    <input
+      type="range"
+      className="slider"
+      min={min}
+      max={max}
+      value={value}
+      onChange={(event) => onChange(event.target.value)}
+    />
   )
 }
