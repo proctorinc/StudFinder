e726ee049cb46c7b928ff2bad7c44f3fb1f0732b
Matt Proctor <proctormatt0@gmail.com>
2023-02-09 22:17:39 -0800

    Added utilities for random occupation and distance

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,9 @@
-import { faker } from "@faker-js/faker";
+export const getRandomDistance = () => {
+  return Math.floor(Math.random() * 500);
+};
 
-export const generateRandomProfiles = () => {
-  const profiles = [];
-
-  for (let i = 0; i < 11; i++) {
-    const gender = Math.floor(Math.random() * 2) === 0 ? "Male" : "Female";
-    profiles.push({
-      image_url: `profile${i + 1}.jpg`,
-      name: faker.name.fullName(),
-      occupation:
-        Math.floor(Math.random() * 2) === 0 ? "Plumber" : "Construction Worker",
-      gender: gender,
-      age: Math.floor(Math.random() * 40) + 25,
-      distance: Math.floor(Math.random() * 500),
-    });
-  }
-  return profiles;
+export const getRandomOccupation = () => {
+  return Math.floor(Math.random() * 2) === 0
+    ? "Plumber"
+    : "Construction Worker";
 };
