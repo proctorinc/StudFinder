acb26fbadd4ad258899d29e8450b68ed22a39715
Matt Proctor <proctormatt0@gmail.com>
2023-02-11 00:24:32 -0800

    Fixed overlapping UI issues

--- src/pages/RateProfile/RateProfile.jsx
+++ src/pages/RateProfile/RateProfile.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion"
 
 import { Slider } from "@/components/ui/Slider";
 import useProfiles from "@/hooks/useProfiles";
@@ -13,21 +14,37 @@ const RateProfile = () => {
   const { currentProfile, getNextProfile, isLoading } = useProfiles();
   const [rating, setRating] = useState(INITIAL_RATING);
   const [disabled, setDisabled] = useState(false);
+  const [ready, setReady] = useState(false);
 
   const handleRating = () => {
     setDisabled(true);
     getNextProfile();
-    setRating(INITIAL_RATING);
   };
-
-  if (isLoading) {
-    return <></>;
-  }
 
   return (
     <div className={styles.container}>
-      <WallBackground animateOnChange={currentProfile.name} />
-      <Profile profile={currentProfile} setDisabled={setDisabled} />
+        {!ready || isLoading ? (
+          <>
+          <WallBackground animateOnChange={ready} initialAnimation={false} />
+          <AnimatePresence mode="popLayout">
+            <motion.div
+              key={ready}
+              style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", height: "75vh", textAlign: "center", maxWidth: "500px", padding: "10px" }}
+              exit={{ x: "-100vw", transition: { duration: 1 } }}
+            >
+              <h2>Welcome to StudFinder!</h2>
+              <p>The dating app for construction and plumbing professionals. "Put the pipe in love" and find someone who "nails" your perfect match.</p>
+              <Button title="Find your Stud" onClick={() => setReady(true)} />
+              <p>\/ Use the stud finder to rate a profile from 1-5 \/</p>
+            </motion.div>
+        </AnimatePresence>
+        </>
+        ) : (
+          <>
+            <WallBackground animateOnChange={currentProfile.name} />
+            <Profile profile={currentProfile} setDisabled={setDisabled} />
+          </>
+        )}
       <div
         style={{
           display: "flex",
@@ -35,18 +52,18 @@ const RateProfile = () => {
           alignItems: "center",
           position: "absolute",
           width: "100%",
-          top: "65vh",
-          gap: "1rem",
+          top: "68vh",
+          gap: "0px",
         }}
       >
-        <h2>{rating}</h2>
-        <Button title="Rate" onClick={handleRating} disabled={disabled} />
+        <h2 style={{ marginBottom: "0" }}>{rating}</h2>
         <Slider
           value={rating}
           min={1}
           max={5}
           onChange={(value) => setRating(value)}
         />
+        {ready && !isLoading && <Button title="Rate" onClick={handleRating} disabled={disabled} />}
       </div>
     </div>
   );
