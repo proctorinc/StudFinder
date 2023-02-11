25f1eb27941d21d5fb3d1f470e2d0e45ae7c3487
Matt Proctor <proctormatt0@gmail.com>
2023-02-11 00:24:12 -0800

    Added ability to disable initial animation

--- src/components/ui/WallBackground/WallBackground.jsx
+++ src/components/ui/WallBackground/WallBackground.jsx
@@ -2,19 +2,28 @@ import { AnimatePresence, motion } from "framer-motion";
 
 import styles from "./WallBackground.module.css";
 
-export const WallBackground = ({ animateOnChange }) => {
+export const WallBackground = ({ animateOnChange, initialAnimation=true }) => {
+  
+  let initial = { x: "100vw" }
+  let animate = {
+    x: "0%",
+    y: "50%",
+    translateY: "-50%",
+    transition: { ease: "easeInOut", duration: 1 },
+  }
+
+  if (!initialAnimation) {
+    initial = {}
+    animate = {}
+  }
+  
   return (
     <AnimatePresence mode="popLayout">
       <motion.div
         className={styles.background}
         key={animateOnChange}
-        initial={{ x: "100vw" }}
-        animate={{
-          x: "0%",
-          y: "50%",
-          translateY: "-50%",
-          transition: { ease: "easeInOut", duration: 1 },
-        }}
+        initial={initial}
+        animate={animate}
         exit={{ x: "-100vw", transition: { duration: 1 } }}
       />
     </AnimatePresence>
