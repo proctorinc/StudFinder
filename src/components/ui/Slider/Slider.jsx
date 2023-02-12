f8dc3c52ce6aadbe881f5e4d486d6740cdf3415b
Matt Proctor <proctormatt0@gmail.com>
2023-02-11 16:33:56 -0800

    Updated Slider size and added rating number marks

--- src/components/ui/Slider/Slider.jsx
+++ src/components/ui/Slider/Slider.jsx
@@ -2,13 +2,22 @@ import "./Slider.css";
 
 export const Slider = ({ value, min, max, onChange }) => {
   return (
-    <input
-      type="range"
-      className="slider"
-      min={min}
-      max={max}
-      value={value}
-      onChange={(event) => onChange(event.target.value)}
-    />
+    <>
+      <div className="track">
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p>5</p>
+      </div>
+      <input
+        type="range"
+        className="slider"
+        min={min}
+        max={max}
+        value={value}
+        onChange={(event) => onChange(event.target.value)}
+      />
+    </>
   );
 };
