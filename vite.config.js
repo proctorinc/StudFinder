54b119fe489efef13a075d9175662170c910632f
Matt Proctor <proctormatt0@gmail.com>
2023-02-11 17:37:58 -0800

    updated base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/studfinder/",
+  base: "/StudFinder/",
   plugins: [react()],
   test: {
     globals: true,
