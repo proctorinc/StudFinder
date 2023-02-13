9deed5cfa8dbbe213406cb58937fed65fe13f3eb
Matt Proctor <proctormatt0@gmail.com>
2023-02-12 16:50:12 -0800

    Added app container styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { Navbar } from "@/components/ui/Navbar";
 import RateProfiles from "@/pages/RateProfile/RateProfile";
 import { ProfilesProvider } from "@/context/ProfilesContext";
 
+import styles from "./App.module.css";
+
 function App() {
   return (
     <ProfilesProvider>
-      <Navbar />
-      <RateProfiles />
+      <div className={styles.appContainer}>
+        <Navbar />
+        <RateProfiles />
+      </div>
     </ProfilesProvider>
   );
 }
